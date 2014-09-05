6fea6c671cc245f4aa4f93a64bf6df5cda680d47
n8chz <n8chz@yahoo.ca>
2014-09-05 00:49:21 -0400

    sdk 1.17

--- data/getcolor.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("click", function(event) {
-  var t = event.target;
-  if (t.nodeName == "TD") {
-   style=t.getAttribute("style");
-   self.port.emit("color-sel",t.getAttribute("style"));
-  }
- }, false);
